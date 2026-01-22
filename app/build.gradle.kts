e660de5521d31b38d7266998dfe5d614bb4cb0d9
paulross24 <paul_ross24@hotmail.com>
2026-01-22 16:19:57 +0000

    Bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "uk.co.rosshome.pumpkin"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.3.0"
+        versionCode = 20
+        versionName = "0.3.2"
     }
 
     buildTypes {
